8c993daf415812f9166d39fe241b06a725bace9f
Nikolay Kavretsky <kavretsky@gmail.com>
2023-05-21 00:01:20 +0400

    Initial Commit

---
+++ Day Activities/Day_ActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_ActivitiesApp.swift
+//  Day Activities
+//
+//  Created by Nikolay Kavretsky on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Day_ActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
